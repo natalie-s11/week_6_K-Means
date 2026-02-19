ce56b0d0d86a5680109b4d7fa5544967e8a70f8a
Brian Wright <bwright.wright6@gmail.com>
2026-02-19 16:28:59 +0000

    added in class code for kmeans clustering

---
+++ in_class.py
@@ -0,0 +1,34 @@
+# %%
+# load libraries
+import pandas as pd
+import numpy as np
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+# %%
+# load data
+
+# %%
+# take a look at the data
+
+# %%
+# separate out the numeric features
+
+# %%
+# documentation for kmeans in sklearn
+
+# %% build a kmeans model
+
+
+# %% look at the information in the model
+
+# %%
+# add the cluster labels to the original data frame
+
+# %%
+  
+# %% simple plot of the clusters
+help(plt.scatter)
+ 
+# %%
+
+
