7684c978d953cedec96bebe0e1f674d45c9288e5
natalie-s11 <teg9gu@virginia.edu>
2026-02-24 08:51:01 -0500

    Implement KMeans clustering and inertia plotting
    Added KMeans clustering model and inertia analysis.

--- in_class.py
+++ in_class.py
@@ -4,31 +4,56 @@ import pandas as pd
 import numpy as np
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
+
 # %%
 # load data
+df = pd.read_csv("house_votes_Dem.csv",encoding='latin-1')
 
 # %%
 # take a look at the data
+df.info()
 
 # %%
 # separate out the numeric features
+c_num = df[["aye", "nay", "other"]]
 
 # %%
 # documentation for kmeans in sklearn
+help(KMeans)
 
-# %% build a kmeans model
+# %% 
+# build a kmeans model
+kmeans = KMeans(n_clusters=3, random_state=42, verbose=1)
+kmeans.fit(c_num)
 
-
-# %% look at the information in the model
+# %% 
+# look at the information in the model
+print(kmeans.cluster_centers_)
+print(kmeans.labels_)
 
 # %%
 # add the cluster labels to the original data frame
+df['cluster'] = kmeans.labels_
 
-# %%
-  
-# %% simple plot of the clusters
+# %% 
+# simple plot of the clusters
 help(plt.scatter)
- 
+
 # %%
-
-
+# use a foor loop to check different cluster
+# numbers and see how the inertia changes
+inertias = []
+k_values = range(1,10)
+for k in k_values:
+    kmeans = KMeans(n_clusters=k, random_state=42)
+    kmeans.fit(c_num)
+    inertias.append(kmeans.inertia_)
+  
+# %%
+# plot the inertia values to see if there is an elbow in the plot
+plt.figure(figsize=(10,5))
+plt.plot(k_values, inertias, marker='o')
+plt.xlabel('Number of Clusters (k)')
+plt.ylabel('Inertia')
+plt.show
+# %%
